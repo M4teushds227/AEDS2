4716fa669b6f2be67f435bd693fc57dadfe0f92d
Mateus Henrique Duarte Silva <mateus.silva.1458093@sga.pucminas.br>
2024-03-04 16:12:13 -0300

    Modularizando e otimizando

--- TP1/alteracao/alter.java
+++ TP1/alteracao/alter.java
@@ -1,39 +1,44 @@
 import java.util.Random;
 
-class alter {   
+class alter {
     @SuppressWarnings("static-access")
-    public static void main (String[] args){
+    public static void main(String[] args) {
+        Random gerador = new Random();
+        gerador.setSeed(4);
         MyIO my = new MyIO();
         int tamanho;
         boolean i = true;
-        String frase;
-        Random gerador = new Random();
-        gerador.setSeed(4);
+        String frase, fraseC;
         while (i) {
             frase = my.readLine();
             tamanho = frase.length();
 
-            if(frase.charAt(0)=='F' && frase.charAt(1) == 'I' && frase.charAt(2) == 'M'){
+            if (frase.charAt(0) == 'F' && frase.charAt(1) == 'I' && frase.charAt(2) == 'M') {
                 i = false;
-            }else{
-                String resp ="";
-                char orig = (char)('a' + (Math.abs(gerador.nextInt()) % 26));
-                char  troca= (char)('a' + (Math.abs(gerador.nextInt()) % 26));
-                for(int x = 0; x < tamanho;x++){
-                    char tempC;
-                    int tempI;
-                    if(frase.charAt(x) == orig ){
-                        tempC = troca;
-                        tempI = tempC;
-                        tempC = (char) tempI;
-                        resp += tempC;
-                    }else{
-                        resp += frase.charAt(x);
-                    }
-                }
-                my.println(resp);
+            } else {
+                fraseC = cifra(frase, tamanho , gerador);
+                my.println(fraseC);
             }
-            
+
         }
+    }
+
+    public static String cifra(String frase, int tamanho, Random gerador) {
+        String resp = "";
+        char orig = (char) ('a' + (Math.abs(gerador.nextInt()) % 26));
+        char troca = (char) ('a' + (Math.abs(gerador.nextInt()) % 26));
+        for (int x = 0; x < tamanho; x++) {
+            char tempC;
+            int tempI;
+            if (frase.charAt(x) == orig) {
+                tempC = troca;
+                tempI = tempC;
+                tempC = (char) tempI;
+                resp += tempC;
+            } else {
+                resp += frase.charAt(x);
+            }
+        }
+        return resp;
     }
 }
