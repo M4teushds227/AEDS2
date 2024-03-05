e0c47f20c252f2fedd6362bb7a33afc40f6ab62e
Mateus Henrique Duarte Silva <mateus.silva.1458093@sga.pucminas.br>
2024-03-05 16:38:47 -0300

    Atv is completa, sem a nota do verde pq ele caiu

--- TP1/is/is.java
+++ TP1/is/is.java
@@ -8,51 +8,106 @@ class is {
         while (i) {
             frase = my.readLine();
             tamanho = frase.length();
-            try {
-                if (frase.charAt(0) == 'F' && frase.charAt(1) == 'I' && frase.charAt(2) == 'M') {
-                    i = false;
+            if (frase.charAt(0) == 'F' && frase.charAt(1) == 'I' && frase.charAt(2) == 'M') {
+                i = false;
+            } else {
+                x1 = verV(frase, tamanho);
+                x2 = verC(frase, tamanho);
+                x3 = verNi(frase, tamanho);
+                x4 = verNr(frase, tamanho);
+
+                if (x1) {
+                    my.print("SIM ");
                 } else {
-                    x1 = verV(frase,tamanho);
-                    x2 = verC(frase,tamanho);
-                    x3 = verNi(frase,tamanho);
+                    my.print("NAO ");
                 }
-            } catch (Exception e) {
+                if (x2) {
+                    my.print("SIM ");
+                } else {
+                    my.print("NAO ");
+                }
+                if (x3) {
+                    my.print("SIM ");
+                } else {
+                    my.print("NAO ");
+                }
+                if (x4) {
+                    my.println("SIM");
+                } else {
+                    my.println("NAO");
+                }
 
             }
+
         }
     }
 
     static boolean verV(String f, int tamanho) {
         boolean resp = true;
-        tamanho = f.length();
         for (int x = 0; x < tamanho; x++) {
-            if (f.charAt(x) == 'a' || f.charAt(x) == 'e' || f.charAt(x) == 'i' || f.charAt(x) == 'o'
+            if (((int) f.charAt(x)) == 'a' || f.charAt(x) == 'e' || f.charAt(x) == 'i' || f.charAt(x) == 'o'
                     || f.charAt(x) == 'u' || f.charAt(x) == 'A' || f.charAt(x) == 'E' || f.charAt(x) == 'I'
                     || f.charAt(x) == 'O' || f.charAt(x) == 'U') {
             } else {
+                x = tamanho;
                 resp = false;
             }
         }
         return resp;
     }
 
-    static boolean verC(String f,int tamanho) {
+    static boolean verC(String f, int tamanho) {
         boolean resp = true;
         for (int x = 0; x < tamanho; x++) {
-            if (!(verV(f,tamanho)) && f.charAt(x) >= 'b' && f.charAt(x) <= 'z' && f.charAt(x) >= 'B' && f.charAt(x) <= 'Z') { 
+            if (((f.charAt(x) >= 'a' && f.charAt(x) <= 'z') || (f.charAt(x) >= 'B' && f.charAt(x) <= 'Z'))
+                    && !(((int) f.charAt(x)) == 'a' || f.charAt(x) == 'e' || f.charAt(x) == 'i' || f.charAt(x) == 'o'
+                            || f.charAt(x) == 'u' || f.charAt(x) == 'A' || f.charAt(x) == 'E' || f.charAt(x) == 'I'
+                            || f.charAt(x) == 'O' || f.charAt(x) == 'U')) {
             } else {
+                x = tamanho;
                 resp = false;
             }
         }
         return resp;
     }
 
-    static boolean verNi(String f,int tamanho){
+    static boolean verNi(String f, int tamanho) {
         boolean resp = true;
-        
+        for (int x = 0; x < tamanho; x++) {
+            if (((int) f.charAt(x)) < 48 || ((int) f.charAt(x)) > 57) {
+                resp = false;
+                x = tamanho;
+            }
+        }
 
+        return resp;
+    }
 
-        
+    static boolean verNr(String f, int tamanho) {
+        boolean resp = false;
+        int verP = 0;
+        for (int i = 0; i < tamanho; i++) {
+            if (f.charAt(i) == ',' || f.charAt(i) == '.') {
+                i = tamanho;
+                for (int x = 0; x < tamanho; x++) {
+                    resp = true;
+                    if (f.charAt(x) == 44 || f.charAt(x) == 46) {
+                        if(verP == 0){
+                            verP ++;
+                        }else{
+                            x = tamanho;
+                            resp = false;
+                        }
+                    } else {
+                        if (f.charAt(x) >= 48 && f.charAt(x) <= 57) {
+                        } else {
+                            x = tamanho;
+                            resp = false;
+                        }
+                    }
+                }
+            }
+        }
         return resp;
     }
 }
