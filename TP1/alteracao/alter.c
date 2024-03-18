d71b760a60ddd01dd80d6bde521cc3cce20b142e
Mateus Henrique Duarte Silva <mateus.silva.1458093@sga.pucminas.br>
2024-03-18 15:12:22 -0300

    atv alteração em c feita

---
+++ TP1/alteracao/alter.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+void *alter(char c[], int i, char orig, char troca);
+
+int main()
+{
+
+    bool i = true;
+    char c[1000];
+    srand(4);
+    while (i)
+    {
+        char orig = 'a' + (abs(rand()) % 26);
+        char troca = 'a' + (abs(rand()) % 26);
+        scanf(" %[^\n]", &c);
+        if (c[0] == 'F' && c[1] == 'I' && c[2] == 'M')
+        {
+            i = false;
+        }
+        else
+        {
+            alter(c, 0, orig, troca);
+        }
+    }
+    return 0;
+}
+
+void *alter(char c[], int i, char orig, char troca)
+{
+
+    if (i == strlen(c))
+    {
+        printf("\n");
+    }
+    else
+    {
+        if (c[i] == orig)
+        {
+            c[i] = troca;
+        }
+        printf("%c", c[i]);
+        alter(c, i + 1, orig, troca);
+    }
+}
