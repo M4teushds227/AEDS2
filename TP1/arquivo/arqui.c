5e87896a8ea5aab6097604cd161f9bd8c3994668
Mateus Henrique Duarte Silva <1458093@sga.pucminas.br>
2024-03-19 14:41:21 -0300

    att de arquivo em c

--- TP1/arquivo/arqui.c
+++ TP1/arquivo/arqui.c
@@ -1,43 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void lerE(int total);
-void abrirC();
+void abrirC(int total);
 
 int main()
 {
     int numr;
     scanf("%d/n", &numr);
     lerE(numr);
-    abrirC();
+    abrirC(numr);
     return 0;
 }
 
 void lerE(int total)
 {
     FILE *arquivo = fopen("arq.txt", "w+");
-    for (int i = 0; i < total; i++){
-        char numlido[100];
-        scanf("%[^\n]", numlido);
-        fprintf(arquivo,"%s\n",numlido);
+    for (int i = 0; i < total; i++)
+    {
+        double numlido;
+        scanf("%lf", &numlido);
+        fprintf(arquivo, "%f\n", numlido);
     }
     fclose(arquivo);
 }
 
-void abrirC(){
+void abrirC(int total)
+{
+
     FILE *arquivo = fopen("arq.txt", "r");
-    fseek(arquivo, 0, SEEK_END);
-    long n = ftell(arquivo);
-    while (n > 0)
+    for (int i = total; i <= 0; i--)
     {
-        n-=8;
-        fseek(arquivo,n,SEEK_SET);
-        double valor = fgetc(arquivo);
-        if(valor == (long) valor){
-            printf("%d",(int) valor);
-        }else{
-            printf("%lf", valor);
+    fseek(arquivo, i, SEEK_SET);
+        float valor;
+        fscanf(arquivo, "%f", &valor);
+        printf("%f\n", valor);
+        if (valor == (long)valor)
+        {
+            printf("%f\n", (int)valor);
+        }
+        else
+        {
+            printf("%f\n", valor);
         }
     }
+
     fclose(arquivo);
-    
 }
